2f6f7f08e2ed36bdc9d051cbef19a93a285dff44
shanbin cai <shanbin.cai@gmail.com>
2019-04-12 16:00:27 +0800

    build basic fundation

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+var ROOT_PATH = path.resolve(__dirname);
+var SRC_PATH = path.resolve(ROOT_PATH, 'src');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'dist');
+
+module.exports = {
+    entry: {
+        index: path.resolve(SRC_PATH, 'index.jsx')
+    },
+    output: {
+        path: BUILD_PATH,
+        filename: 'js/[name].[hash:5].js'
+    },
+    //开启dev source map
+    devtool: 'eval-source-map',
+    //开启 webpack dev server
+    devServer: {
+        historyApiFallback: true,
+        hot: true,
+        inline: true, //实时刷新
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            // {
+            //     test: /\.jsx?$/,
+            //     loaders: ['eslint-loader'],
+            //     include: SRC_PATH,
+            // }, {
+            {
+                test: /\.jsx?$/,
+                loaders: ['babel-loader'],
+                include: SRC_PATH,
+                exclude: path.resolve(ROOT_PATH, 'node_modules')
+            }
+        ]
+    },
+    // 配置plugin
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': '"development"'
+        }),
+        // new webpack.optimize.UglifyJsPlugin({
+        //     sourceMap: true,
+        // }),
+        new HtmlWebpackPlugin({
+            title: 'cbreno',
+            filename: 'index.html',
+            template: path.resolve(SRC_PATH, 'templates', 'index.html'),
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeRedundantAttributes: true,
+                removeScriptTypeAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                removeAttributeQuotes: true
+            }
+        })
+    ],
+    optimization: {
+        minimize: false,
+        minimizer: [
+            new UglifyJsPlugin({
+                uglifyOptions: {
+                    compress: false
+                }
+            })
+        ]
+    }
+};
